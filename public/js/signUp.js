b7a040f4fd24a35a44d12d29fc5734f0d99e81f3
Ayush mishra <ayushmishraam8@gmail.com>
2024-11-15 14:58:41 +0530

    chnages

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   }
   const tokenSaved = (token) => {
-    document.cookie = `token=${id}; path=/;`;
+    document.cookie = `token=${token}; path=/;`;
   };
   function showNotification(color, message) {
     const notification = document.getElementById("notification");
